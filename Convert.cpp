1c81c2240548e5cab5b5c638e311bc3e55645756
kiereiaayman <kiereiaayman@gmail.com>
2024-12-05 13:34:40 +0200

    Update Convert.cpp
    implementation of convertToJSON function and treeToJson function

--- Convert.cpp
+++ Convert.cpp
@@ -26,4 +26,41 @@ string convertSiblings(TreeNode *node, int indentLevel) {
     }
     return str;
 }
+string convertToJSON(TreeNode *node, int indentLevel) {
+    string str = "";
+    string indent = createIndent(indentLevel);
+
+    if (node->children.size() == 0) {
+        str += "\"" + node->TagName + "\": \"" + node->TagValue + "\"";
+    } else {
+        if (node->children.size() == 1 || node->children[0]->TagName != node->children[1]->TagName) {
+            str += "\"" + node->TagName + "\": {\n";
+            for (int i = 0; i < node->children.size(); i++) {
+                str += indent + "\t";  // Add tab for each child
+                if (i != node->children.size() - 1)
+                    str += convertToJSON(node->children[i], indentLevel + 1) + ",\n";
+                else
+                    str += convertToJSON(node->children[i], indentLevel + 1) + "\n";
+            }
+            str += indent + "}";  // Closing brace with the same indent level
+        } else {
+            str += "\"" + node->TagName + "\": [\n";
+            for (int i = 0; i < node->children.size(); i++) {
+                str += indent + "\t";  // Add tab for each sibling
+                if (i != node->children.size() - 1)
+                    str += convertSiblings(node->children[i], indentLevel + 1) + ",\n";
+                else
+                    str += convertSiblings(node->children[i], indentLevel + 1) + "\n";
+            }
+            str += indent + "]";  // Closing square bracket with the same indent level
+        }
+    }
+    return str;
+}
+
+string treeToJson(Tree *tree) {
+    if (!tree || tree->isEmpty()) return "{}";
+    return "{\n\t" + convertToJSON(tree->getRoot(), 1) + "\n}";  // Start indentation at level 1
+}
+
 
