b2255861c45a5b208720a74457482ea200dabc62
Ahmed-Tarek-Mahmoud <143649528+Ahmed-Tarek-Mahmoud@users.noreply.github.com>
2024-12-13 14:03:32 +0200

    hierarchy edits

--- TreeParse/treeparse.h
+++
@@ -1,8 +0,0 @@
-#ifndef     TREE_PARSE
-#define     TREE_PARSE
-#include "..\tree.h"
-#include <bits/stdc++.h>
-
-TreeNode* parseNode(const string &s,int &i);
-Tree parseTree(const string &s);
-#endif
