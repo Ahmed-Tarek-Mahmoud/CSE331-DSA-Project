6f5f573ffa170b769faa8ab22d2f28933d61ac7e
johnnagy44 <johnnagy123456789@gmail.com>
2024-12-18 01:55:34 +0200

    modifiedMinifier

--- external/minifier.hpp
+++ external/minifier.hpp
@@ -8,14 +8,11 @@
  * @brief Minifies an XML file by removing unnecessary whitespace and newlines outside of tags or data fields.
  *
  * This function reads an input XML file, removes extra spaces and newlines outside of meaningful content,
- * and writes the minified version to an output file.
+ * and return the minified version as a string.
  *
- * If the specified output file does not exist, the function automatically creates
- * a new file with the same name and a `.xml` extension.
  *
  * @param inputFile A string containing the path to the input XML file. The file must exist and be readable.
- * @param outputFile A string containing the desired path for the output XML file. If the file does not exist,
- *        a new file with the same name and a `.xml` extension is created.
+ * @return A string containing the minified XML content.
  *
  * @note
  * - Spaces inside tags (e.g., between attributes) and within data fields are preserved.
@@ -23,12 +20,32 @@
  *
  *
  * // Function Call
- * minifyXML("input.xml", "output.xml");
+ * minifyXML("input.xml");
  *
+ *
+ * @throws Prints an error message and returns an empty string if:
+ * - The input file cannot be opened.
+ */
+
+string minifyXML(const char *inputFile);
+
+/**
+ * @brief Minifies an XML file and writes the result to an output file.
+ *
+ * This function reads an input XML file, minifies its content, and writes the minified XML to the specified
+ * output file. If the output file does not exist, it is automatically created. If it already exists,
+ * its content is overwritten.
+ *
+ * @param inputFile A string containing the name of the input XML file. The file must exist and be readable.
+ * @param outputFile A string containing the name of the output XML file. If the file does not exist, it will be created.
+ *
+ * @note
+ * - This function calls `minifyXML_modified` to perform the minification.
+ * - Any errors related to file operations (e.g., read/write issues) are printed to `stderr`.
  *
  * @throws Prints an error message and exits the function if:
  * - The input file cannot be opened.
  * - The output file cannot be created or written to.
  */
 
-void minifyXML(const char *inputFile, const char *outputFile);
+void minifyXMLFiles(const char *inputFile, const char *outputFile);
