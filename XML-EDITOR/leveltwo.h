3540492d638b3c3051292a1491a141014b296122
kiereiaayman <kiereiaayman@gmail.com>
2024-12-26 22:47:51 +0200

    qui Level 2

--- XML-EDITOR/leveltwo.h
+++ XML-EDITOR/leveltwo.h
@@ -2,6 +2,7 @@
 #define LEVELTWO_H
 
 #include <QMainWindow>
+#include <string>
 
 namespace Ui {
 class LevelTwo;
@@ -18,8 +19,17 @@ public:
 private slots:
     void on_tabWidget_tabBarClicked(int index);
 
+    void on_BrowseAnalysis_clicked();
+
+    void on_gobtn_clicked();
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
 private:
     Ui::LevelTwo *ui;
+    QString saveFile;
+    QString filePath;  // Qstring version of the path
+    std::string fileStrPath;
 };
 
 #endif // LEVELTWO_H
